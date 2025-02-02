842abd8921460cf0faafbf8575214c4a67d46c22
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-02-03 00:48:36 +0200

    feat: integrate Hero component into App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Hero from "./components/Hero";
+
 const App = () => {
   return (
-    <main>
-      <h1 className="text-5xl text-violet-300 ">Welcome to Awwwards</h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
     </main>
   );
 };
