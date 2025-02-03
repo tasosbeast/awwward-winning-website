98909ebb0fef8b57bd852744fedc26e0581cc8fb
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-02-04 00:23:08 +0200

    feat: add background section to App layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
+
+      <section className="min-h-screen z-0 bg-blue-500" />
     </main>
   );
 };
