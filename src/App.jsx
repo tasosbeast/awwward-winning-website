f07f7fc575d8be7777bbedc6ae9272659514b846
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-02-04 21:26:53 +0200

    feat: add About component and integrate it into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
-
-      <section className="min-h-screen z-0 bg-blue-500" />
+      <About />
     </main>
   );
 };
