de2b17d682b0307e4d2174f9f48ac32dd25b41f9
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-02-04 22:01:19 +0200

    feat: enhance About component with new layout and content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,22 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div id="about" className="min-h-screen min-w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <h2 className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to zentry
+        </h2>
+        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-8xl">
+          Disc<b>o</b>ver The World&apos;s <br /> largest shared <b>a</b>
+          dventure
+        </div>
+
+        <div className="about-subtext">
+          <p>The Game of Games begins-your life, now an epic MMORPG</p>
+          <p>Zentry unites every player from countless games and platforms</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
