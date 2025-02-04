9d78adcbd5103d5e0a1b8cef125c2db8d8ae4aba
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-02-04 21:00:38 +0200

    fix: replace div with button for better accessibility in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -55,7 +55,6 @@ const Hero = () => {
             }
           },
         });
-
         gsap.from("#current-video", {
           transformOrigin: "center center",
           scale: 0,
@@ -104,7 +103,7 @@ const Hero = () => {
       >
         <div>
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-            <div
+            <button
               onClick={handleMiniVdClick}
               className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
               role="button"
