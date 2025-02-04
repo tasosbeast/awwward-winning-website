8f9fae42c43b58be55817b913adf2356262fbdea
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-02-04 21:01:05 +0200

    fix: add button type attribute for improved accessibility in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -105,8 +105,8 @@ const Hero = () => {
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
             <button
               onClick={handleMiniVdClick}
+              type="button"
               className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
-              role="button"
             >
               <video
                 ref={nextVideoRef}
