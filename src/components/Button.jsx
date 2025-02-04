23411796aaff689eebef2aa67e0ca36e24a7d0e2
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-02-04 20:58:50 +0200

    fix: remove unnecessary line break in Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -10,7 +10,6 @@ const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
       <span className="relative inline-flex overflow-hidden font-general text-xs uppercase">
         <div>{title}</div>
       </span>
-
       {rightIcon}
     </button>
   );
